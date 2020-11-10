d1e433bc20b8f634243aac9f64cbe128a0879107
huulinh99 <43189610+huulinh99@users.noreply.github.com>
2020-11-10 18:08:08 +0700

    Revert "Revert "commit for FE""

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import './App.css';
 import Home from './pages/Home';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch,useRouteMatch,useParams } from 'react-router-dom';
 import Navbar from './components/Navbar'
 import AboutCoffee from './pages/AboutCoffee';
 import News from './pages/News';
 import Product from './pages/Product';
+import Login from './pages/Login';
 import Order from './pages/Order';
 import ViewCard from './pages/ViewCard';
+import firebase from 'firebase';
+import firebaseConfig from "./services/firebase.config";
+import ProductDetail from './pages/ProductDetail';
+
+firebase.initializeApp(firebaseConfig);
+
+export const AuthContext = React.createContext(null);
+
 function App() {
   return (   
     <Router>
@@ -18,6 +27,10 @@ function App() {
         <Route path="/Product" component={Product}/>
         <Route path="/Order" component={Order}/>
         <Route path="/ViewCard" component={ViewCard}/>
+        <Route path="/Login" component={Login}/>
+        <Route path='/products/:productId' render={(props) => {
+                    return ( <ProductDetail {...props } /> )
+                }}/>
       </Switch>
       </div>      
     </Router>     
