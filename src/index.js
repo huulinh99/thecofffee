61f455c083eb458e6495d44bb7e60292f81fbf38
huulinh99 <43189610+huulinh99@users.noreply.github.com>
2020-11-10 18:07:42 +0700

    Revert "Revert "Dev.khanhmhq""

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import Order from './pages/Order';
 import Product from './pages/Product';
+import Blog from './components/Blog'
+import News from './pages/News';
 import Home from './pages/Home';
 import AboutCoffee from './pages/AboutCoffee';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -11,9 +13,17 @@ import $ from 'jquery';
 import Popper from 'popper.js';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './components/store/reducers/rootReducers';
+
+const store = createStore(rootReducer);
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={store}>
+      <News />
+    </Provider>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
