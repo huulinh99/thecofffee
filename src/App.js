0c94c5b03dd4ba81479962026fc54c82c0f4888b
linhnhse130563 <huulinhnguyen99@gmail.com>
2020-11-10 16:54:37 +0700

    commit for FE

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import './App.css';
 import Home from './pages/Home';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch,useRouteMatch,useParams } from 'react-router-dom';
 import Navbar from './components/Navbar'
 import AboutCoffee from './pages/AboutCoffee';
 import Product from './pages/Product';
+import Login from './pages/Login';
 import Order from './pages/Order';
 import ViewCard from './pages/ViewCard';
+import firebase from 'firebase';
+import firebaseConfig from "./services/firebase.config";
+import ProductDetail from './pages/ProductDetail';
+
+firebase.initializeApp(firebaseConfig);
+
+export const AuthContext = React.createContext(null);
+
 function App() {
   return (   
     <Router>
@@ -17,6 +26,10 @@ function App() {
         <Route path="/Product" component={Product}/>
         <Route path="/Order" component={Order}/>
         <Route path="/ViewCard" component={ViewCard}/>
+        <Route path="/Login" component={Login}/>
+        <Route path='/products/:productId' render={(props) => {
+                    return ( <ProductDetail {...props } /> )
+                }}/>
       </Switch>
       </div>      
     </Router>     
