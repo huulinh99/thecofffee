f9ae00507a208c4f712b54ee64b3f835419c1ccf
linhnhse130563 <huulinhnguyen99@gmail.com>
2020-11-11 19:56:20 +0700

    sdfsdf

--- server/server.js
+++ server/server.js
@@ -7,6 +7,6 @@ app.use(express.static(publicPath));
 app.get('*', (req, res) => {
    res.sendFile(path.join(publicPath, 'index.html'));
 });
-app.listen(process.env.PORT || 3000, function(){
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
+app.listen(port, () => {
+    console.log('Server is up!');
+ });
